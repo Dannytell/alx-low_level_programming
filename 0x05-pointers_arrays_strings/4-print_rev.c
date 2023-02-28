3448783a3c52e4aca83d11d7c70e0fba026ddad8
Dannytell <dannytello001@gmail.com>
2023-02-28 15:49:05 +0300

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,9 @@
-#include "main"
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
 void print_rev(char *s)
 {
 int longi = 0;
@@ -16,3 +21,5 @@ s--;
 }
 _putchar('\n');
 }
+
+
